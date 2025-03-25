b4c505d9c8988d7e2248073ada6b03d9ea2ee458
Kenil Kavar <125490448+Kenil-kavar@users.noreply.github.com>
2025-03-25 20:02:06 +0530

    Update urls.py
    Added route for career form

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib import admin
-from .views import handle_audio, submit_form # Import the view
+from .views import handle_audio, submit_form, Career_form 
 
 
 urlpatterns = [
@@ -8,6 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('handle-audio/', handle_audio, name='handle_audio'),
     path('submit/', submit_form, name='submit_form'),
+    path('career-submit/', Career_form, name='Career_form'),
+
 ]
 
 
