d892af79db54082cc3790dc431b97c135cd62b03
Kenil Kavar <125490448+Kenil-kavar@users.noreply.github.com>
2025-03-25 22:31:59 +0530

    Update urls.py
    Added the routes of enquire form

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from django.contrib import admin
-from .views import handle_audio, submit_form, Career_form 
+from .views import handle_audio, submit_form, Career_form , Enquire_Form
 
 
 urlpatterns = [
-    # Route for handling audio files
     path('admin/', admin.site.urls),
     path('handle-audio/', handle_audio, name='handle_audio'),
     path('submit/', submit_form, name='submit_form'),
     path('career-submit/', Career_form, name='Career_form'),
+    path("enquire-form-submit/", Enquire_Form, name="Enquire_Form"),
 
 ]
 
