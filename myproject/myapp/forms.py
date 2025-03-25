575db70cc3ef6b6056701486d6a9037e92dd442c
Kenil Kavar <125490448+Kenil-kavar@users.noreply.github.com>
2025-03-25 19:27:23 +0530

    Update forms.py
    Added the mobile number and password fields in the code

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 
 class YourForm(forms.Form):
-    name = forms.CharField(max_length=100)
+    fullname = forms.CharField(max_length=100)
+    mobilenumber = forms.CharField(max_length=15)  # Assuming mobile numbers are strings
     email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+    password = forms.CharField(widget=forms.PasswordInput)
